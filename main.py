970998af657fcf7433f34fb0037ab843a41bb48b
Eugene Storozhuk <deus.lo.vult@mail.ru>
2023-11-15 23:09:29 +0500

    note: only works with display scale 100% otherwise pixels in record doesn't match pixels in exetuc

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from pynput import keyboard
 import sys
 import time
 import enum
-from abc import ABC, abstractmethod, abstractproperty, abstractclassmethod, abstractstaticmethod
+from abc import ABC, abstractmethod
+from functools import reduce
 
 SEPARATOR = '\t'
 
@@ -21,10 +22,11 @@ class Recorder:
     last_event_time = -1
     file_path = None
     file = None
+    should_stop = False
     listening = True
     # for stop listening
-    exitKey = keyboard.Key.esc
-    keysPressed = {exitKey: False,
+    exit_key = keyboard.Key.esc
+    keysPressed = {exit_key: False,
                    keyboard.Key.ctrl: False}
 
     def __init__(self, file_path, is_using_time):
@@ -54,8 +56,10 @@ class Recorder:
         command_line = [command.value]
         for arg in args[:-1]:
             command_line.append(arg.value if isinstance(arg, enum.Enum) else arg)
+        if self.using_time:
+            command_line.append(self.get_time_passed())
         self.file.write(SEPARATOR.join(map(str, command_line)) + '\n')
-    def logger_wrapper_factory(is_keyboard=False):
+    def logger_wrapper_factory(*, is_keyboard):
         def logger_wrapper(func):
             def wrapper(self, *args, **kwargs):
                 self.logger(COMMAND.KEYBOARD if is_keyboard else COMMAND.MOUSE, args)
@@ -66,17 +70,19 @@ class Recorder:
         return logger_wrapper
 
     def check_exit_state(self):
-        if self.keysPressed[self.exitKey] and self.keysPressed[keyboard.Key.ctrl]:
+        if self.keysPressed[self.exit_key] and self.keysPressed[keyboard.Key.ctrl]: # check exit condition
+            self.should_stop = True
+        elif self.should_stop and not any(self.keysPressed.values()): # check if 'all' keys released | note: 'all' means two keys in this implementation, ctrl and exit_key
             self.listening = False
 
-    @logger_wrapper_factory(True)
+    @logger_wrapper_factory(is_keyboard=True)
     def keyboard_on_event(self, key: keyboard.Key, is_pressed):
         # я растроен, что в match-case не нужно использование break
         match key:
             case keyboard.Key.ctrl | keyboard.Key.ctrl_l | keyboard.Key.ctrl_r:
                 self.keysPressed[keyboard.Key.ctrl] = is_pressed
                 self.check_exit_state()
-            case self.exitKey:
+            case self.exit_key:
                 self.keysPressed[key] = is_pressed
                 self.check_exit_state()
 
@@ -86,16 +92,16 @@ class Recorder:
     def keyboard_on_release(self, key):
         self.keyboard_on_event(key, False)
 
-    @logger_wrapper_factory(False)
+    @logger_wrapper_factory(is_keyboard=False)
     def mouse_on_click(self, x, y, button, pressed):
-        pass  # print(f'{button} {pressed} at {(x, y)}')
+        pass
 
     def listen(self):
         Mlistener = mouse.Listener(on_click=self.mouse_on_click)
         Klistener = keyboard.Listener(on_press=self.keyboard_on_press,
                                       on_release=self.keyboard_on_release)
 
-        with open(self.file_path, 'wt', encoding='ascii') as file:
+        with open(self.file_path, 'wt', encoding='utf-8') as file:
             self.file = file
             Mlistener.start()
             Klistener.start()
@@ -110,10 +116,10 @@ class Reader:
         self.file_path = file_path
 
     def read(self):
-        with open(self.file_path, 'rt', encoding='ascii') as file:
+        with open(self.file_path, 'rt', encoding='utf-8') as file:
             for line in file:
                 self.handle(line)
-            print('--------------all--------------')
+            print('Complete!')
 
     def handle(self, line):
         command = line[:-1].split(SEPARATOR)
@@ -131,9 +137,9 @@ class Action(ABC):
             case COMMAND.MOUSE_DOWN:
                 return MouseAction(True, *command)
             case COMMAND.KEYBOARD_UP:
-                return KeyboardAction(False, *command, 0)
+                return KeyboardAction(False, *command)
             case COMMAND.MOUSE_UP:
-                return MouseAction(False, *command, 0)
+                return MouseAction(False, *command)
             case _:
                 raise NotImplementedError(f'{command[0]} in {command} is unexpected')
 
@@ -145,12 +151,13 @@ class Action(ABC):
 class MouseAction(Action):
     controller = mouse.Controller()
 
-    def __init__(self, is_pressed, x, y, button, dtime):
+    def __init__(self, is_pressed, x, y, button, dtime=0):
         self.is_pressed = is_pressed
         self.x = int(x)
         self.y = int(y)
         self.button = mouse.Button(eval(button))
-        self.dtime = int(dtime)
+        global times_faster
+        self.dtime = int(dtime)/1000/times_faster
 
     def execute(self):
         time.sleep(self.dtime)
@@ -159,7 +166,6 @@ class MouseAction(Action):
             self.controller.press(self.button)
         else:
             self.controller.release(self.button)
-        print(f"{'Pressed' if self.is_pressed else 'Released'} {self.button} at {self.x},{self.y}")
 
     def move_to(self, x, y):
         self.controller.position = x, y
@@ -168,10 +174,10 @@ class MouseAction(Action):
 class KeyboardAction(Action):
     controller = keyboard.Controller()
 
-    def __init__(self, is_pressed, key, dtime):
+    def __init__(self, is_pressed, key, dtime=0):
         self.is_pressed = is_pressed
         self.key = keyboard.KeyCode(int(key[1:-1])) if key[0] == '<' else key[1:-1]
-        self.dtime = int(dtime)
+        self.dtime = int(dtime)/1000/times_faster
 
     def execute(self):
         time.sleep(self.dtime)
@@ -179,28 +185,53 @@ class KeyboardAction(Action):
             self.controller.press(self.key)
         else:
             self.controller.release(self.key)
-        print(f"{'Pressed' if self.is_pressed else 'Released'} {self.key}")
 
 
 if __name__ == '__main__':
+    class ARGUMENTS(enum.Enum):
+        RECORD = 'w'
+        EXECUTE = 'r'
+        TIME = 't'
+
+    # is_using_time = False
+    # times_faster = 1
     match len(sys.argv):
         case 0:
-            print('\n', 'Первым аргументом обычно (в windows) указан путь до файла программы', '\n')
+            print('\n', 'Нулевым аргументом обычно (в windows) указан путь до файла программы', '\n')
             exit(0)
         case 1:
-            print('\n', 'Вторым аругментом укажите имя файла для записи/воспроизведения макроса', '\n')
+            print('\n', 'Первым аругментом укажите имя файла для записи/воспроизведения макроса', '\n')
             exit(0)
         case 2:
-            print('\n', 'Третьим аргументом укажите "w" для записи или "r" для чтения', '\n')
+            print('\n', 'Вторым аргументом укажите "{0}" для записи или "{1}" для чтения'.format(
+                ARGUMENTS.RECORD, ARGUMENTS.EXECUTE), '\n')
             exit(0)
+        case 3:
+            match ARGUMENTS(sys.argv[2]):
+                case ARGUMENTS.RECORD:
+                    is_using_time = False
+                    # times_faster = 1
+                    print(f'Третьим аргументом укажите "{ARGUMENTS.TIME}" для учета времени между кликами')
+                case ARGUMENTS.EXECUTE:
+                    print('\n', 'Третьим аргументом укажите число - кол-во раз в которое будет ускорено время между кликами', '\n')
+                    exit(0)
+        case 4:
+            if sys.argv[3] == ARGUMENTS.TIME:
+                is_using_time = True
+            elif sys.argv[3].replace('.','').isdigit():
+                times_faster = float(sys.argv[3])
+                times_faster = times_faster if times_faster != 0 else 1
+            else:
+                is_using_time = False
+                times_faster = 1
 
     path = sys.argv[1]
-    readWriteArg = sys.argv[2]
 
-    if readWriteArg == 'r':
-        reader = Reader(path)
-        reader.read()
-    elif readWriteArg == 'w':
-        recorder = Recorder(path, is_using_time=True)
-        print('\n', 'Для выхода из программы зажмите ctrl +', recorder.exitKey.name, '\n')
-        recorder.listen()
+    match ARGUMENTS(sys.argv[2]):
+        case ARGUMENTS.RECORD:
+            recorder = Recorder(path, is_using_time=True)
+            print('\n', 'Для выхода из программы зажмите ctrl +', recorder.exit_key.name, '\n')
+            recorder.listen()
+        case ARGUMENTS.EXECUTE:
+            reader = Reader(path)
+            reader.read()
